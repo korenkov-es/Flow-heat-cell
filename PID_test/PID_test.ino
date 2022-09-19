8d550cc0b99a4630e4df0c063bfdf15f68b142f1
korenkov-es <korenkov.es@phystech.edu>
2022-09-19 13:45:46 +0300

    Added equations file, added fillets to temp sensor support

--- PID_test/PID_test.ino
+++ PID_test/PID_test.ino
@@ -1,13 +1,13 @@
 
 #include "GyverPID.h"
-GyverPID regulator(2, 0, 0, 1000);
+GyverPID regulator(10, 0, 1, 1500);
 
-#define SETPOINT 50.0
+#define SETPOINT 80.0
 
 
 #include <microDS18B20.h>
-
-MicroDS18B20<A5> sensor1;
+MicroDS18B20<A4> sensor1;
+MicroDS18B20<A5> sensor2;
 
 
 void setup() {
@@ -16,23 +16,38 @@ void setup() {
   regulator.setDirection(NORMAL); // направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
   regulator.setLimits(0, 255);    // пределы (ставим для 8 битного ШИМ). ПО УМОЛЧАНИЮ СТОЯТ 0 И 255
   regulator.setpoint = SETPOINT;
+  pinMode(3, OUTPUT);
   delay(500);
 }
 
 void loop() {
-  static uint32_t tmr = millis();
-  static bool flag;
-
+  
   sensor1.requestTemp();
-  delay(750);
+  sensor2.requestTemp();
+  delay(1000);
 
 
   regulator.input = sensor1.getTemp();
   regulator.getResultTimer();
   analogWrite(3, regulator.output);
-  
-  Serial.print(regulator.input);
-  Serial.print(' ' );
+
+
+  if (sensor1.readTemp()) {
+	  Serial.print(sensor1.getTemp());
+  }
+  else {
+	  Serial.print("Error Middle");
+  }
+  Serial.print(' ');
+
+  if (sensor2.readTemp()) {
+	  Serial.print(sensor2.getTemp());
+  }
+  else {
+	  Serial.print("Error Edge");
+  }
+  Serial.print(' ');
+
   Serial.println(regulator.output);
   
 }
